96a0abbec7fb6e7833974904725e99e935613490
Tomas Duclos <duclosiw@gmail.com>
2022-01-16 02:24:13 -0300

    removing header if the pathname starts with /auth

--- client/src/components/header/Left.tsx
+++ client/src/components/header/Left.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 
+// Next
+import Link from "next/link";
+
 const Left = () => {
     return (
         <div>
-            <img
-                className="iw-w-12 iw-h-12 iw-object-cover iw-object-center iw-rounded-full"
-                src="/img/logo.svg"
-                alt="logo"
-            />
+            <Link href="/">
+                <a className="iw-block iw-w-12 iw-h-12">
+                    <img
+                        className="iw-w-full iw-h-full iw-object-cover iw-object-center iw-rounded-full"
+                        src="/img/logo.svg"
+                        alt="logo"
+                    />
+                </a>
+            </Link>
+            
         </div>
     )
 }
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,19 @@
+// Next
+import { useRouter } from "next/router";
+
 // Components
 import Header from "components/header/Header";
 
 import "css/App.css";
 
 const App = ({ Component, pageProps }) => {
+    const router = useRouter();
+
     return (
         <>
-        <Header/>
+        {
+            !router.pathname.startsWith("/auth") && <Header/>
+        }
         <Component { ...pageProps } />
         </>
     )
